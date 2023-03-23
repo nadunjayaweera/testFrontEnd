ac4c8a092be99f505c2465ab466b8ef782e366b9
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-03-23 16:02:30 +0530

    macke changes in user list

--- src/Source/Admin/Customers/Userlist.js
+++ src/Source/Admin/Customers/Userlist.js
@@ -1,87 +1,110 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Title from '../Dashboard/title';
+import { alpha, styled } from '@mui/material/styles';
+import { DataGrid, gridClasses } from '@mui/x-data-grid';
 
-// Generate Order Data
-function createData(id, createdate, firstName, lastName, email) {
-  return { id, createdate, firstName, lastName, email };
-}
+const ODD_OPACITY = 0.2;
 
-const rows = [
-  createData(
-    0,
-    '3 March, 2023',
-    'Nadun',
-    'Jayaweera',
-    'nadunmj@gmail.com',
-    
-  ),
-  createData(
-      1,
-    '3 March, 2023', 
-    'Sadaru',
-    'DeSilva',
-    'sadarudesilva@gmail.com',
-    
-  ),
-  createData(
-      2,
-      '4 March, 2023', 
-      'Pramila',
-      'Krishan',
-      'pramilakrishan@gmail.com',
-     
-  ),
-  createData(
-    3,
-    '4 March, 2023', 
-    'Malaka',
-    'Yayakodi',
-    'malakajayakodi@gmail.com',
-    
-  ),
-  createData(
-    4,
-    '5 March, 2023', 
-    'Kaweesha',
-    'Thilakarathna',
-    'kaweeshath@gmail.com',
-    
-  ),
+const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+  [`& .${gridClasses.row}.even`]: {
+    backgroundColor: theme.palette.grey[200],
+    '&:hover, &.Mui-hovered': {
+      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
+      '@media (hover: none)': {
+        backgroundColor: 'transparent',
+      },
+    },
+    '&.Mui-selected': {
+      backgroundColor: alpha(
+        theme.palette.primary.main,
+        ODD_OPACITY + theme.palette.action.selectedOpacity,
+      ),
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          ODD_OPACITY +
+            theme.palette.action.selectedOpacity +
+            theme.palette.action.hoverOpacity,
+        ),
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            ODD_OPACITY + theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  },
+}));
+
+const dummyUsers = [
+  {
+    id: 1,
+    createdate: '3 March, 2023',
+    firstname: 'Nadun',
+    lastname: 'Jayaweera',
+    email: 'nadunmj@gmail.com',
+  },
+  {
+    id: 2,
+    createdate: '3 March, 2023',
+    firstname: 'Sadaru',
+    lastname: 'DeSilva',
+    email: 'sadarudesilva@gmail.com',
+  },
+  {
+    id: 3,
+    createdate: '4 March, 2023',
+    firstname: 'Pramila',
+    lastname: 'Krishan',
+    email: 'pramilakrishan@gmail.com',
+  },
 ];
 
-// function preventDefault(event) {
-//   event.preventDefault();
-// }
-
 export default function Users() {
+  const [editRowsModel, setEditRowsModel] = React.useState({});
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'createdate', headerName: 'Create Date', width: 200 },
+    {
+      field: 'firstname',
+      headerName: (
+        <div style={{ textAlign: 'left' }}>
+          Fist Name
+        </div>
+      ),
+      width: 130
+    },
+    {
+      field: 'lastname',
+      headerName: (
+        <div style={{ textAlign: 'left' }}>
+          Last Name 
+        </div>
+      ),
+      width: 130
+    },
+    {
+      field: 'email',
+      headerName: (
+        <div style={{ textAlign: 'left' }}>
+          Email
+        </div>
+      ),
+      width: 200
+    },  
+];
+  
   return (
-    <React.Fragment>
-      <Title>Users</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Create Date</TableCell>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
-            <TableCell>Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-                  <TableCell>{row.createdate}</TableCell>
-              <TableCell>{row.firstName}</TableCell>
-              <TableCell>{row.lastName}</TableCell>
-              <TableCell>{row.email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </React.Fragment>
+    
+    <div style={{ height: 400, width: '100%' }}>
+      <StripedDataGrid
+        rows={dummyUsers}
+        columns={columns}
+        editRowsModel={editRowsModel}
+        onEditRowsModelChange={(newModel) => setEditRowsModel(newModel)}
+      />
+    </div>
   );
 }
