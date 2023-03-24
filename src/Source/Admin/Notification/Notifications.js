11e665056ccf061e9dc0033ab44be42380468b8f
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-03-24 20:25:50 +0530

    Add dialog box to notification

--- src/Source/Admin/Notification/Notifications.js
+++ src/Source/Admin/Notification/Notifications.js
@@ -1,40 +1,59 @@
+import { useState } from 'react';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
 
 const NotificationMessage = ({ notification }) => {
   return (
     <div>
       <p>{notification.message}</p>
-      <p>{notification.timestamp.toString()}</p>
     </div>
   );
 };
 
-const Notificationicon = () => {
+const NotificationIcon = () => {
   const [notification, setNotification] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleNotificationClick = () => {
     setNotification({
-      message: 'This is a dummy notification!',
-      timestamp: new Date(),
+      message: 'Dairy Milk Quantity Alert!',
+      
       isRead: false
     });
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <div>
       <IconButton color="inherit">
-        <Badge badgeContent={2} color="secondary">
+        <Badge badgeContent={1} color="secondary">
           <NotificationsIcon onClick={handleNotificationClick} />
         </Badge>
       </IconButton>
-      {notification && <NotificationMessage notification={notification} />}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Notification</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {notification && <NotificationMessage notification={notification} />}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
 
-export default Notificationicon;
+export default NotificationIcon;
