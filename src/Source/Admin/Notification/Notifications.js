acc28bfc3aed2d0b6da61d32c4f250f79a0b5b1a
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-03-24 19:59:49 +0530

    Notification bar commit

--- src/Source/Admin/Notification/Notifications.js
+++ src/Source/Admin/Notification/Notifications.js
@@ -1,17 +1,40 @@
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-function Notificationicon() {
-    return (
-        <div>
-            <IconButton color="inherit">
-                 <Badge badgeContent={2} color="secondary">
-                     <NotificationsIcon />
-                </Badge>
-            </IconButton>
-        </div>
-    )
-}
+const NotificationMessage = ({ notification }) => {
+  return (
+    <div>
+      <p>{notification.message}</p>
+      <p>{notification.timestamp.toString()}</p>
+    </div>
+  );
+};
+
+const Notificationicon = () => {
+  const [notification, setNotification] = useState(null);
+
+  const handleNotificationClick = () => {
+    setNotification({
+      message: 'This is a dummy notification!',
+      timestamp: new Date(),
+      isRead: false
+    });
+  };
+
+  return (
+    <div>
+      <IconButton color="inherit">
+        <Badge badgeContent={2} color="secondary">
+          <NotificationsIcon onClick={handleNotificationClick} />
+        </Badge>
+      </IconButton>
+      {notification && <NotificationMessage notification={notification} />}
+    </div>
+  );
+};
 
 export default Notificationicon;
